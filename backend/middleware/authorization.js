544aa0b8edd3e6822889c586168c7b96a1006fe2
Jerzy Łoba <jloba@protonmail.com>
2021-06-12 00:14:17 +0200

    proper way of handling authorization message

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -2,14 +2,12 @@ module.exports = async (req, res, next) => {
   try {
     const { role } = req;
     if (role !== "admin") {
-      return res.status(403).json({ message: "Not authorized" });
+      return res.status(403).send("Not authorized");
     }
 
     next();
   } catch (err) {
     console.error(err.message);
-    res.status(403).json({
-      message: "Not authorized",
-    });
+    res.status(403).jsend("Not authorized");
   }
 };
